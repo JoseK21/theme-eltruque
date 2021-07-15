8b491f72336ae940541fda57fb17938433e1879a
JoseK21 <josek21nunez@gmail.com>
2021-07-14 22:29:15 -0600

    first commit

---
+++ sidebar-footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @package Sydney
+ */
+?>
+
+
+	<?php //Set widget areas classes based on user choice
+		$widget_areas = get_theme_mod('footer_widget_areas', '3');
+		if ($widget_areas == '3') {
+			$cols = 'col-md-4';
+		} elseif ($widget_areas == '4') {
+			$cols = 'col-md-3';
+		} elseif ($widget_areas == '2') {
+			$cols = 'col-md-6';
+		} else {
+			$cols = 'col-md-12';
+		}
+	?>
+
+	<div id="sidebar-footer" class="footer-widgets widget-area" role="complementary">
+		<div class="container">
+			
+
+		</div>	
+	</div>
